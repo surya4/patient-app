bc1427020fec4dccdc1a2fb1345b88740de6236b
Surya Singh <me@suryasingh.xyz>
2017-09-28 19:38:46 +0530

    post

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var details = require('./routes/details');
+var done = require('./routes/done');
 
 var app = express();
 
@@ -18,12 +19,13 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/details', details);
+app.use('/done', done);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,17 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Patient App' });
 });
 
-router.post('/', function(req, res, next) {
-    let data = JSON.stringify(req.body);
-    console.log(data);
+router.post('/done', function(req, res, next) {
+    try {
+        let data = JSON.stringify(req.body);
+        console.log(data);
+        res.send(data);
+
+    } catch (e) {
+        console.log("Error : " + e);
+        return next(e);
+    }
+
 });
 
 module.exports = router;
